45aa0a94c9f36abf6b0bc2709573e5ffb788b7cc
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-11 20:40:32 +0900

    feat: SetNameCore 구현

--- Adego-iOS/Source/Feature/Auth/SetName/SetNameCore.swift
+++ Adego-iOS/Source/Feature/Auth/SetName/SetNameCore.swift
@@ -9,20 +9,49 @@ import ComposableArchitecture
 
 @Reducer
 struct SetNameCore: Reducer {
+    @Dependency(\.flow) var flow
     
     @ObservableState
     struct State: Equatable {
+        var isFormValid: Bool = false
+        var stringLength: Int = 0
+        var name: String = ""
         
+        public init() {}
     }
     
-    enum Action: Equatable {
-        
+    enum Action: ViewAction {
+        case navigateToSetProfileImage
+        case view(View)
+
     }
     
-    func reduce(into state: inout State, action: Action) -> Effect<Action> {
-        let newState = state
-        switch action {
-            
+    @CasePathable
+    public enum View: BindableAction, Sendable {
+        case binding(BindingAction<State>)
+    }
+    
+    public init() {}
+    
+    var body: some Reducer<State, Action> {
+        BindingReducer(action: \.view)
+        Reduce { state, action in
+            switch action {
+            case .navigateToSetProfileImage:
+                flow.push(
+                    SetProfileImageView(
+                        store: Store(
+                            initialState: SetProfileImageCore.State()
+                        ) {
+                            SetProfileImageCore()
+                        }
+                    )
+                )
+                return .none
+            case .view(.binding):
+                state.isFormValid = !state.name.isEmpty
+                return .none
+            }
         }
     }
     // MARK: - func
