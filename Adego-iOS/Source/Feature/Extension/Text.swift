92fcbd085c996515639e379576ebc9b25000d56d
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-11 20:35:52 +0900

    feat: add custom placeholderStyle func

---
+++ Adego-iOS/Source/Feature/Extension/Text.swift
@@ -0,0 +1,16 @@
+//
+//  Text.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 4/9/24.
+//
+
+import SwiftUI
+
+extension Text {
+    func placeholderStyle() -> Text {
+        self
+            .foregroundColor(Color(.gray40))
+            .font(.wantedSans())
+    }
+}
