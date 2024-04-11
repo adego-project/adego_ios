6678da35018d0182c8dcfc930423eb4237c46bf9
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-11 20:36:50 +0900

    feat: MainView UI 구현, 지도 추가, Preview 추가

--- Adego-iOS/Source/Feature/Main/MainView.swift
+++ Adego-iOS/Source/Feature/Main/MainView.swift
@@ -6,9 +6,82 @@
 //
 
 import SwiftUI
+import MapKit
+import ComposableArchitecture
 
+@ViewAction(for: MainCore.self)
 struct MainView: View {
-    var body: some View {
-        Text("")
+    @Perception.Bindable var store: StoreOf<MainCore>
+    
+    init(store: StoreOf<MainCore>) {
+        self.store = store
     }
+    
+    var body: some View {
+        WithPerceptionTracking {
+            ZStack {
+                Map(coordinateRegion: .constant(store.mapRegion ?? MKCoordinateRegion()),
+                    showsUserLocation: true,
+                    annotationItems: locations,
+                    annotationContent: { location in
+                    MapAnnotation(coordinate: location.coordinates) {
+                        MapAnnotationView()
+                            .scaleEffect(store.mapLocation == location ? 1 : 0.7)
+                            .clipShape(
+                                Circle()
+                            )
+                            .overlay(
+                                Circle()
+                                .stroke()
+                                .foregroundStyle(.gray80)
+                            )
+                            .onTapGesture {
+                                store.send(.updateMapRegion(location))
+                            }
+                    }
+                })
+                .preferredColorScheme(.dark)
+                .ignoresSafeArea(.all)
+                .onAppear {
+                    store.send(.findCurrentLocation)
+                }
+                VStack {
+                    Spacer()
+                    
+                    locationsPreviewStack
+                }
+            }
+        }
+    }
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(locations) { location in
+                PromisePreviewView(
+                    store: Store(initialState: PromisePreviewCore.State()) {
+                        PromisePreviewCore()
+                    }, location:  locations.first!
+                )
+                .frame(maxWidth: .infinity)
+                .transition(
+                    .asymmetric(
+                        insertion: .move(edge: .trailing),
+                        removal: .move(edge: .leading)
+                    )
+                )
+            }
+            .onAppear {
+                print("locationsPreviewStack")
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView(
+        store: Store(
+            initialState: MainCore.State()
+        ) {
+            MainCore()
+        }
+    )
 }
