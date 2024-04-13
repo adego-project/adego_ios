607b8e561be6e9183b94d9433a6e49c2a314d00a
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-14 03:19:18 +0900

    feat: add CustomInputTextFieldView

---
+++ Adego-iOS/Source/Feature/Components/CustomInputTextFieldView.swift
@@ -0,0 +1,29 @@
+//
+//  CustomInputTextFieldView.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 4/12/24.
+//
+
+import SwiftUI
+
+struct CustomInputTextFieldView: View {
+    let text: String
+    let input: Binding<String>
+    let placeholder: String
+    
+    var body: some View {
+        Text(text)
+            .foregroundStyle(.gray60)
+            .padding(.top, 40)
+        
+        TextField("",
+                  text: input,
+                  prompt: Text(placeholder)
+                    .placeholderStyle()
+        )
+            .frame(width: 343)
+            .padding(.top, 4)
+            .foregroundStyle(.gray100)   
+    }
+}
