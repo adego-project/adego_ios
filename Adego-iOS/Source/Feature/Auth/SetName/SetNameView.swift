f7ac51acf287f04926f7b05382fe00a70db1dc1d
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-14 03:16:13 +0900

    feat: add CustomInputTextFieldView

--- Adego-iOS/Source/Feature/Auth/SetName/SetNameView.swift
+++ Adego-iOS/Source/Feature/Auth/SetName/SetNameView.swift
@@ -13,10 +13,10 @@ struct SetNameView: View {
     @Perception.Bindable var store: StoreOf<SetNameCore>
     
     public init(store: StoreOf<SetNameCore>) {
-      self.store = store
+        self.store = store
     }
     
-
+    
     var body: some View {
         WithPerceptionTracking {
             VStack(alignment: .leading) {
@@ -24,18 +24,12 @@ struct SetNameView: View {
                     .font(.wantedSans(24, weight: .regular))
                     .foregroundStyle(.white)
                     .padding(.top, 76)
-                Text("사용자 이름 (\(store.stringLength)/8)")
-                    .foregroundStyle(.gray60)
-                    .padding(.top, 40)
                 
-                TextField("", 
-                          text: $store.name,
-                          prompt: Text("전화번호를 입력해주세요")
-                            .placeholderStyle()
+                CustomInputTextFieldView(
+                    text: "사용자 이름 (\(store.nameLength)/8)",
+                    input: $store.name,
+                    placeholder: "전화번호를 입력해주세요"
                 )
-                    .frame(width: 343)
-                    .padding(.top, 4)
-                    .foregroundStyle(.gray100)
                 
                 Rectangle()
                     .frame(width: 343, height: 1)
@@ -47,29 +41,23 @@ struct SetNameView: View {
                     store.send(.navigateToSetProfileImage)
                 } label: {
                     Text("다음으로 넘어가기 \(Image(systemName: "arrow.right"))")
-                        .font(.wantedSans(weight: .midium))
-                        .foregroundStyle(.gray10)
-                        .frame(width: 343, height: 56)
-                        .background(.gray100)
                 }
-                .clipShape(
-                    RoundedRectangle(cornerRadius: 8)
-                )
+                .buttonStyle(CustomWhiteRoundedButton())
                 .padding(.bottom, 10)
                 
             }
         }
-//        .toolbar(content: {
-//            ToolbarItem(placement: .navigationBarLeading) {
-//                Button {
-//                    store.send(.navigateToSetProfileImage)
-//                } label: {
-//                    Image(systemName: "arrow.backward")
-//                        .foregroundColor(.gray50)
-//                        .font(.title3)
-//                }
-//            }
-//        })
+        //        .toolbar(content: {
+        //            ToolbarItem(placement: .navigationBarLeading) {
+        //                Button {
+        //                    store.send(.navigateToSetProfileImage)
+        //                } label: {
+        //                    Image(systemName: "arrow.backward")
+        //                        .foregroundColor(.gray50)
+        //                        .font(.title3)
+        //                }
+        //            }
+        //        })
     }
 }
 
