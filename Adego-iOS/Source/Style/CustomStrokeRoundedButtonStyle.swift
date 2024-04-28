1bda235fa5dfed812c681cceb82a09fd6f020fcd
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-28 19:11:08 +0900

    feat: CustomStrokeRoundedButtonStyle

---
+++ Adego-iOS/Source/Style/CustomStrokeRoundedButtonStyle.swift
@@ -0,0 +1,22 @@
+//
+//  CustomStrokeRoundedButtonStyle.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 4/12/24.
+//
+
+import SwiftUI
+
+struct CustomStrokeRoundedButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background(Color.gray.opacity(0.2))
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke()
+                    .foregroundColor(.gray)
+            )
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+    }
+}
