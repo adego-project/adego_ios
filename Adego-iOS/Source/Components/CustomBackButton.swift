855eadd47ed6bbcd099cccce023d36595592851f
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-04-29 22:17:14 +0900

    feat: Components

---
+++ Adego-iOS/Source/Components/CustomBackButton.swift
@@ -0,0 +1,27 @@
+//
+//  CustomBackButton.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 4/7/24.
+//
+
+import UIKit
+
+class CustomNavigationBarController: UINavigationController {
+    
+        override func viewDidLoad() {
+            customNavigationBar()
+        }
+    
+        func customNavigationBar() {
+            let navigationBarAppearance = UINavigationBarAppearance()
+    
+            navigationBarAppearance.configureWithOpaqueBackground()
+            navigationBarAppearance.backgroundColor = .black
+            navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+    
+            navigationBar.standardAppearance = navigationBarAppearance
+            navigationBar.scrollEdgeAppearance = navigationBarAppearance
+            navigationBar.tintColor = .gray50
+        }
+    }
