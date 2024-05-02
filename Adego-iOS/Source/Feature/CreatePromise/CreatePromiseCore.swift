90c0128d71f67b7365e2e766b4b1a944b4c233db
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-05-02 10:51:29 +0900

    feat: CreatePromiseCore

---
+++ Adego-iOS/Source/Feature/CreatePromise/CreatePromiseCore.swift
@@ -0,0 +1,64 @@
+//
+//  CreatePromiseCore.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 4/12/24.
+//
+
+import ComposableArchitecture
+import FlowKit
+
+@Reducer
+struct CreatePromiseCore: Reducer {
+    @Dependency(\.flow) var flow
+    
+    @ObservableState
+    struct State: Equatable {
+        var isFormValid: Bool = false
+        var promiseTitle: String = "" {
+            didSet {
+                titleLength = promiseTitle.count
+            }
+        }
+        var titleLength: Int = 0 {
+            didSet {
+                if titleLength > 12 {
+                    isFormValid = true
+                } else {
+                    isFormValid = false
+                }
+            }
+        }
+    }
+    
+    enum Action: ViewAction {
+        case navigateToSelectDayView
+        case view(View)
+    }
+    
+    @CasePathable
+    public enum View: BindableAction, Sendable {
+        case binding(BindingAction<State>)
+    }
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer(action: \.view)
+        Reduce { state, action in
+            switch action {
+            case .navigateToSelectDayView:
+                flow.push(
+                    SelectDayView(
+                        store: Store(
+                            initialState: SelectDayCore.State()
+                        ) {
+                            SelectDayCore()
+                        }
+                    )
+                )
+                return .none
+            case .view(.binding):
+                return .none
+            }
+        }
+    }
+}
