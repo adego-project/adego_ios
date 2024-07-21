68e3f2e637125b4c0ea95622a3ea7f6b9263da3d
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-07-21 13:37:08 +0900

    create: ErrorResponse

---
+++ Adego-iOS/Source/Domain/Entities/Error/ErrorResponse.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorResponse.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 7/9/24.
+//
+
+import Foundation
+
+struct ErrorResponse: Error, Codable {
+    let statusCode: Int?
+    let message: String?
+}
