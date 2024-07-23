cc3e7b9b714ca47f5500b42a2e5f94a158ae8202
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-07-24 00:00:26 +0900

    Create Place.swift

---
+++ Adego-iOS/Source/Domain/Entities/Promise/Place.swift
@@ -0,0 +1,17 @@
+//
+//  Place.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 7/14/24.
+//
+
+import Foundation
+
+struct Place: Codable {
+    let id: String
+    let name: String
+    let address: String
+    let x: String
+    let y: String
+    let planId: String
+}
