c660ebc1083349ae2ee7687262b621ecaf19d25d
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-07-23 23:59:41 +0900

    Update SigninCore.swift

--- Adego-iOS/Source/Presentation/View/Auth/Signin/SigninCore.swift
+++ Adego-iOS/Source/Presentation/View/Auth/Signin/SigninCore.swift
@@ -17,34 +17,100 @@ struct SigninCore: Reducer {
     }
     
     enum Action: Equatable {
-        case navigateToSetName
+        case successSigninWithApple(String)
         case kakaoLogin
     }
     
-    var body: some ReducerOf<Self> {
+    var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
+            case .successSigninWithApple(let appleToken):
+                let authRepository = AuthRepositoryImpl()
+                let signinUseCase = SigninUseCase(authRepository: authRepository)
                 
-            case .navigateToSetName:
-                
-                flow.push(
-                    SetNameView(
-                        store: Store(
-                            initialState: SetNameCore.State()
-                        ) {
-                            SetNameCore()
-                        }
-                    )
-                )
+                signinUseCase.signin(appleToken: appleToken) { result in
+                    switch result {
+                    case .success(let tokens):
+                        print("✅Access Token: \(tokens.accessToken)")
+                        print("✅Refresh Token: \(tokens.refreshToken)")
+                        
+                        let saveAccessToken = KeychainManager.shared.save(key: "accessToken", string: tokens.accessToken)
+                        let saveRefreshToken = KeychainManager.shared.save(key: "refreshToken", string: tokens.refreshToken)
+                        tokenRefresh(accessToken: tokens.refreshToken)
+                        getUser(accessToken: tokens.accessToken)
+                    case .failure(let error):
+                        print("🚫Sign in failed: \(error.localizedDescription)")
+                        print(error)
+                    }
+                }
                 return .none
             case .kakaoLogin:
                 
                 return .none
             }
+            
+            // MARK: - func
+            func tokenRefresh(
+                accessToken: String
+            ) {
+                let authRepository = AuthRepositoryImpl()
+                let signinUseCase = SigninUseCase(authRepository: authRepository)
+                signinUseCase.tokenRefresh(accessToken: accessToken) {  result in
+                    switch result {
+                    case .success(let tokens):
+                        let saveAccessToken = KeychainManager.shared.save(key: "accessToken", string: tokens.accessToken)
+                        print("✅tokenRefresh.Access Token: \(tokens.accessToken)")
+                    case .failure(let error):
+                        print("🚫SigninCore.tokenRefresh error: \(error.localizedDescription)")
+                        print(error)
+                    }
+                }
+            }
+            
+            func getUser(
+                accessToken: String
+            ) {
+                let userRepository = UserRepositoryImpl()
+                let getUserUseCase = UserUseCase(userRepository: userRepository)
+                getUserUseCase.getUser(accessToken: accessToken) { result in
+                    switch result {
+                    case .success(let info):
+                        print("✅SigninCore.getUser id:", info.id)
+                        print("✅SigninCore.getUser name:", info.name ?? "")
+                        print("✅SigninCore.getUser planId:", info.planId)
+                        print("✅SigninCore.getUser profileImage:", info.profileImage ?? "")
+                        if ((info.name?.isEmpty) != nil) {
+                            flow.push(
+                                SetNameView(
+                                    store: Store(
+                                        initialState: SetNameCore.State()
+                                    ) {
+                                        SetNameCore()
+                                    }
+                                )
+                            )
+                        } else {
+                            flow.replace(
+                                [
+                                    MainView(
+                                        store: Store(
+                                            initialState: MainCore.State()
+                                        ) {
+                                            MainCore()
+                                        }
+                                    )
+                                ]
+                            )
+                        }
+                        
+                    case .failure(let error):
+                        print("🚫SigninCore.getUser error: \(error.localizedDescription)")
+                        print(error)
+                    }
+                }
+            }
         }
     }
-    // MARK: - func
-    
 }
 
 
