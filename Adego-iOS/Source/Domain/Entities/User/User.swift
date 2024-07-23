e4037b10bb69bb4209269ad4408a38147ff793f2
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-07-24 00:00:24 +0900

    Create User.swift

---
+++ Adego-iOS/Source/Domain/Entities/User/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 7/12/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: String
+    let name: String?
+    let planId: String?
+    let profileImage: String?
+}
