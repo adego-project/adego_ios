a70b9e8bcbf63e2dd42671c4a6920c005948ccaf
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-08-10 08:09:48 +0900

    feat: SelectTime

--- Adego-iOS/Source/Presentation/View/CreatePromise/Time/SelectTimeCore.swift
+++ Adego-iOS/Source/Presentation/View/CreatePromise/Time/SelectTimeCore.swift
@@ -13,12 +13,13 @@ struct SelectTimeCore: Reducer {
     @ObservableState
     struct State: Equatable {
         var promiseTitle: String = ""
-        
+        var selectTime: String = ""
         var amPM: String = "오전"
         var hours: String = "4"
         var minute: String = "14"
         var selectedDate: String = ""
         var selectedAddress: String = ""
+        var formattedDate: String = ""
     }
     
     enum Action: ViewAction {
@@ -39,10 +40,12 @@ struct SelectTimeCore: Reducer {
         Reduce { state, action in
             switch action {
             case .navigateToSelectLocationView:
+                let formattedTime = formatTime(amPM: state.amPM, hours: state.hours, minute: state.minute)
+                print(formattedTime)
                 flow.push(
                     SelectLocationView(
                         store: Store(
-                            initialState: SelectLocationCore.State(promiseTitle: state.promiseTitle, selectedDate: state.selectedDate)
+                            initialState: SelectLocationCore.State(promiseTitle: state.promiseTitle, selectedDate: state.formattedDate)
                         ) {
                             SelectLocationCore()
                         }
@@ -50,9 +53,29 @@ struct SelectTimeCore: Reducer {
                 )
                 return .none
             case .setValue:
+
                 return .none
             case .view(.binding):
                 return .none
+            }
+            
+            func formatTime(amPM: String, hours: String, minute: String) -> String {
+                guard let intHours = Int(hours) else {
+                    return "Invalid hours"
+                }
+                
+                var formattedHours: String
+                
+                if amPM == "오전" {
+                    formattedHours = intHours == 12 ? "00" : String(format: "%02d", intHours)
+                } else if amPM == "오후" {
+                    formattedHours = intHours == 12 ? "12" : String(format: "%02d", intHours + 12)
+                } else {
+                    return "Invalid AM/PM value"
+                }
+                
+                let formattedTime = "\(state.selectedDate) \(formattedHours):\(minute):00"
+                return formattedTime
             }
         }
     }
