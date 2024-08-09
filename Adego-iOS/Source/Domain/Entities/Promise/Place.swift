92a7d799bdb349631cdb084a36c200c7c6a37d2b
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-08-10 08:11:46 +0900

    add: Equatable

--- Adego-iOS/Source/Domain/Entities/Promise/Place.swift
+++ Adego-iOS/Source/Domain/Entities/Promise/Place.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
-struct Place: Codable {
+struct Place: Codable, Equatable {
     let id: String
     let name: String
     let address: String
     let x: String
     let y: String
     let planId: String
+     
+    init(id: String, name: String, address: String, x: String, y: String, planId: String) {
+        self.id = id
+        self.name = name
+        self.address = address
+        self.x = x
+        self.y = y
+        self.planId = planId
+    }
 }
