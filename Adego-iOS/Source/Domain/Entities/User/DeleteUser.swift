3dd17bae6a803de0590318076f68452a8cc4d23b
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-08-10 08:06:55 +0900

    Create DeleteUser.swift

---
+++ Adego-iOS/Source/Domain/Entities/User/DeleteUser.swift
@@ -0,0 +1,13 @@
+//
+//  DeleteUser.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 8/10/24.
+//
+
+import Foundation
+
+struct DeleteUser: Codable {
+    let status: String
+    let data: [String: String]
+}
