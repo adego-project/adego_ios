4fb60592795c706fc131b0ad3eeb5f6ac0ecbbf9
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-08-17 04:52:58 +0900

    Create UserInfo.swift

---
+++ Adego-iOS/Source/Domain/Repositories/Storage/UserInfo/UserInfo.swift
@@ -0,0 +1,27 @@
+//
+//  UserInfo.swift
+//  Adego-iOS
+//
+//  Created by 최시훈 on 8/12/24.
+//
+
+import Foundation
+
+enum UserInfo {
+    case verificationID
+}
+
+class UserInfoManager {
+    static func save(_ user: User, _ value: String) {
+        UserDefaults.standard.set(value, forKey: String(describing: user))
+    }
+    
+    static func get(_ user: User) -> String? {
+        return UserDefaults.standard.string(forKey: String(describing: user))
+    }
+    
+    static func remove(_ user: User) {
+        UserDefaults.standard.removeObject(forKey: String(describing: user))
+    }
+    
+}
