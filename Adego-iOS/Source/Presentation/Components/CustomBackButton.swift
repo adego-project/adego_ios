94d4b29b1886f0b61f7dde2720ce42a2086c073a
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-08-19 23:09:15 +0900

    Update CustomBackButton.swift

--- Adego-iOS/Source/Presentation/Components/CustomBackButton.swift
+++ Adego-iOS/Source/Presentation/Components/CustomBackButton.swift
@@ -9,19 +9,19 @@ import UIKit
 
 class CustomNavigationBarController: UINavigationController {
     
-        override func viewDidLoad() {
-            customNavigationBar()
-        }
-    
-        func customNavigationBar() {
-            let navigationBarAppearance = UINavigationBarAppearance()
-    
-            navigationBarAppearance.configureWithOpaqueBackground()
-            navigationBarAppearance.backgroundColor = .black
-            navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
-    
-            navigationBar.standardAppearance = navigationBarAppearance
-            navigationBar.scrollEdgeAppearance = navigationBarAppearance
-            navigationBar.tintColor = .gray50
-        }
+    override func viewDidLoad() {
+        customNavigationBar()
     }
+    
+    func customNavigationBar() {
+        let navigationBarAppearance = UINavigationBarAppearance()
+        
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = .black
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        navigationBar.standardAppearance = navigationBarAppearance
+        navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        navigationBar.tintColor = .gray50
+    }
+}
