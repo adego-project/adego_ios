5606bbcbf8bfc53e03916a409157ce855c2b04cc
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-09-11 21:20:35 +0900

    feat: EditView

--- Adego-iOS/Source/Presentation/View/Setting/Edit/EditView.swift
+++ Adego-iOS/Source/Presentation/View/Setting/Edit/EditView.swift
@@ -13,30 +13,32 @@ struct EditView: View {
     
     var body: some View {
         WithPerceptionTracking {
-            CustomInputTextField(
-                text: "사용자 이름 (\(store.nameLength)/8)",
-                input: $store.name,
-                placeholder: "수정할 이름을 입력해주세요",
-                isFormValid: store.isFormValid
-            )
-            
-            if store.isFormValid {
-                Text("글자가 너무 길어요.")
-                    .font(.wantedSans())
-                    .foregroundStyle(.red)
-                    .padding(.top, 10)
+            VStack {
+                CustomInputTextField(
+                    text: "사용자 이름 (1/8)",
+                    input: $store.name,
+                    placeholder: "수정할 이름을 입력해주세요",
+                    isFormValid: store.isFormValid
+                )
+//                
+//                if store.isFormValid {
+//                    Text(store.message)
+//                        .font(.wantedSans())
+//                        .foregroundStyle(.red)
+//                        .padding(.top, 10)
+//                }
+                
+                Spacer()
+                
+                Button {
+                    store.send(.save(store.name))
+                } label: {
+                    Text("저장하기")
+                }
+                .buttonStyle(CustomWhiteRoundedButton())
+                .opacity(store.isFormValid ? 0.6 : 1)
+                .disabled(store.isFormValid ? true : false)
             }
-            
-            Button {
-                store.send(.dismiss)
-            } label: {
-                Text("저장하기")
-            }
-            .buttonStyle(CustomWhiteRoundedButton())
-            .opacity(store.isFormValid ? 0.6 : 1)
-            .disabled(store.isFormValid ? true : false)
-            
-            Spacer()
         }
     }
 }
